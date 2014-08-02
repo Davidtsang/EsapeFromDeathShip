0b9deb1615a18498339460b9f63a471502b19558
david.tsang <david.zen.2007@gmail.com>
2014-08-02 10:36:59 +0800

    beforw web api

---
+++ Escape/ContactServer.h
@@ -0,0 +1,13 @@
+//
+//  ContactServer.h
+//  Escape
+//
+//  Created by user on 14-8-2.
+//  Copyright (c) 2014年 david_tsang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContactServer : NSObject
+
+@end
---
+++ Escape/ContactServer.m
@@ -0,0 +1,13 @@
+//
+//  ContactServer.m
+//  Escape
+//
+//  Created by user on 14-8-2.
+//  Copyright (c) 2014年 david_tsang. All rights reserved.
+//
+
+#import "ContactServer.h"
+
+@implementation ContactServer
+
+@end
